c6b6167f8d1d69065832ae2edf41559d5ce06a88
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2025-10-29 10:30:23 -0300

    feat(frontend): adiciona suporte a PrimeNG e configurações
    Foi adicionado o suporte à biblioteca PrimeNG como dependência do projeto, incluindo novos componentes para estilização e configuração de temas.
    
    Foi implementado um novo arquivo de configuração para o tema Aura, além de uma nova configuração para o PrimeNG, que permite personalizar a tradução e a ativação do efeito ripple.
    
    Foram atualizadas as dependências para incluir as versões mais recentes do Angular e suas bibliotecas associadas.
    
    Foi criado um novo arquivo de configuração de aplicativo para gerenciar provedores e inicializações, melhorando a estrutura geral do ambiente de desenvolvimento.
    
    Foram removidos arquivos obsoletos que não eram mais necessários.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,12 +1,75 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
+import {
+  provideHttpClient,
+  withInterceptors,
+  withXsrfConfiguration,
+} from '@angular/common/http';
+import {
+  ApplicationConfig,
+  LOCALE_ID,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withComponentInputBinding,
+  withInMemoryScrolling,
+  withPreloading,
+  withRouterConfig,
+  withViewTransitions,
+} from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
+import { AURA_CONFIG } from './core/config/aura.config';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
     provideBrowserGlobalErrorListeners(),
-    provideZonelessChangeDetection(),
-    provideRouter(routes)
-  ]
+    provideZoneChangeDetection({
+      eventCoalescing: true,
+      runCoalescing: true,
+    }),
+    provideRouter(
+      routes,
+      withPreloading(PreloadAllModules),
+      withViewTransitions(),
+      withRouterConfig({
+        paramsInheritanceStrategy: 'always',
+        urlUpdateStrategy: 'eager',
+        onSameUrlNavigation: 'reload',
+      }),
+      withComponentInputBinding(),
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled',
+        anchorScrolling: 'enabled',
+      })
+    ),
+    provideAnimations(),
+    {
+      provide: MessageService,
+      useClass: MessageService,
+    },
+    provideHttpClient(
+      withInterceptors([]),
+      withXsrfConfiguration({
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+    providePrimeNG({
+      theme: {
+        preset: AURA_CONFIG,
+        options: {
+          darkModeSelector: '.dark',
+          cssLayer: {
+            name: 'primeng',
+            order: 'primeng',
+          },
+        },
+      },
+    }),
+  ],
 };
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,10 @@
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import 'zone.js';
 import { App } from './app/app';
+import { appConfig } from './app/app.config';
 
-bootstrapApplication(App, appConfig)
-  .catch((err) => console.error(err));
+registerLocaleData(localePt, 'pt-BR');
+
+bootstrapApplication(App, appConfig).catch((err) => console.error(err));
