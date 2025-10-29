f2270e34a1bf7dcaf0bdc164401cd7a43596e109
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2025-10-29 10:56:19 -0300

    style(app.config): otimiza a configuração de estilos
    Foi removido o código desnecessário de importação para melhorar a legibilidade.
    
    Foi adicionado um novo formato para as camadas de CSS, garantindo que a ordem da aplicação dos estilos seja PrimeNG primeiro, seguida pelos estilos do Tailwind.
    
    Uma nova versão do arquivo HTML foi criada, enquanto a anterior foi removida para facilitar os ajustes de design.
    
    Foram feitas melhorias nas configurações do provedor, mantendo a funcionalidade reutilizável e organizando as importações.

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,23 +1,19 @@
+import { provideHttpClient, withInterceptors, withXsrfConfiguration } from '@angular/common/http';
 import {
-  provideHttpClient,
-  withInterceptors,
-  withXsrfConfiguration,
-} from '@angular/common/http';
-import {
-  ApplicationConfig,
-  LOCALE_ID,
-  provideBrowserGlobalErrorListeners,
-  provideZoneChangeDetection,
+	ApplicationConfig,
+	LOCALE_ID,
+	provideBrowserGlobalErrorListeners,
+	provideZoneChangeDetection,
 } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import {
-  PreloadAllModules,
-  provideRouter,
-  withComponentInputBinding,
-  withInMemoryScrolling,
-  withPreloading,
-  withRouterConfig,
-  withViewTransitions,
+	PreloadAllModules,
+	provideRouter,
+	withComponentInputBinding,
+	withInMemoryScrolling,
+	withPreloading,
+	withRouterConfig,
+	withViewTransitions,
 } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import { providePrimeNG } from 'primeng/config';
@@ -25,51 +21,52 @@ import { routes } from './app.routes';
 import { AURA_CONFIG } from './core/config/aura.config';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    { provide: LOCALE_ID, useValue: 'pt-BR' },
-    provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({
-      eventCoalescing: true,
-      runCoalescing: true,
-    }),
-    provideRouter(
-      routes,
-      withPreloading(PreloadAllModules),
-      withViewTransitions(),
-      withRouterConfig({
-        paramsInheritanceStrategy: 'always',
-        urlUpdateStrategy: 'eager',
-        onSameUrlNavigation: 'reload',
-      }),
-      withComponentInputBinding(),
-      withInMemoryScrolling({
-        scrollPositionRestoration: 'enabled',
-        anchorScrolling: 'enabled',
-      })
-    ),
-    provideAnimations(),
-    {
-      provide: MessageService,
-      useClass: MessageService,
-    },
-    provideHttpClient(
-      withInterceptors([]),
-      withXsrfConfiguration({
-        cookieName: 'XSRF-TOKEN',
-        headerName: 'X-XSRF-TOKEN',
-      })
-    ),
-    providePrimeNG({
-      theme: {
-        preset: AURA_CONFIG,
-        options: {
-          darkModeSelector: '.dark',
-          cssLayer: {
-            name: 'primeng',
-            order: 'primeng',
-          },
-        },
-      },
-    }),
-  ],
+	providers: [
+		{ provide: LOCALE_ID, useValue: 'pt-BR' },
+		provideBrowserGlobalErrorListeners(),
+		provideZoneChangeDetection({
+			eventCoalescing: true,
+			runCoalescing: true,
+		}),
+		provideRouter(
+			routes,
+			withPreloading(PreloadAllModules),
+			withViewTransitions(),
+			withRouterConfig({
+				paramsInheritanceStrategy: 'always',
+				urlUpdateStrategy: 'eager',
+				onSameUrlNavigation: 'reload',
+			}),
+			withComponentInputBinding(),
+			withInMemoryScrolling({
+				scrollPositionRestoration: 'enabled',
+				anchorScrolling: 'enabled',
+			}),
+		),
+		provideAnimations(),
+		{
+			provide: MessageService,
+			useClass: MessageService,
+		},
+		provideHttpClient(
+			withInterceptors([]),
+			withXsrfConfiguration({
+				cookieName: 'XSRF-TOKEN',
+				headerName: 'X-XSRF-TOKEN',
+			}),
+		),
+		providePrimeNG({
+			ripple: true,
+			theme: {
+				preset: AURA_CONFIG,
+				options: {
+					darkModeSelector: '.dark',
+					cssLayer: {
+						name: 'primeng',
+						order: 'primeng',
+					},
+				},
+			},
+		}),
+	],
 };
