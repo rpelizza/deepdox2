15ebad02be10dc6f9ff37b6a23a226bed9368db2
Rafael Pedro Pelizza <rafael.pelizza@gmail.com>
2025-10-29 10:47:20 -0300

    chore(config): atualiza configurações do projeto
    Foram alterados os estilos de indentação para tabs com tamanho 4 e adicionada a limitação de comprimento de linha a 120 caracteres no arquivo de configuração do editor.
    
    Foi criado um novo arquivo de configuração do Prettier, incluindo padrões de formatação. Adicionou-se suporte ao Autoprefixer e Tailwind CSS no projeto, além de atualizações nas dependências no package-lock.json, incluindo Prettier e Tailwind.
    
    Foram removidas configurações de cores antigas e não utilizadas do projeto, simplificando a configuração e evitando confusões futuras.
    
    BREAKING CHANGE: A estrutura dos estilos e configurações foi alterada, o que pode exigir ajustes em componentes que dependem dessas configurações.

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{html,ts,scss}'],
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [require('tailwindcss-primeui')],
+};
